f5eb8df695c5eaba6e26905d0e78f40e9be56b3e
Yugesh-kumar26 <kr4780@srmist.edu.in>
2026-03-09 08:46:42 +0530

    UC1:Social Media Username Availability Checker

--- src/Assignment1.java
+++ src/Assignment1.java
@@ -1,2 +1,52 @@
+import java.util.*;
+
 public class Assignment1 {
+
+    private HashMap<String, Integer> userMap = new HashMap<>();
+    private HashMap<String, Integer> attemptCount = new HashMap<>();
+
+    public boolean checkAvailability(String username) {
+        attemptCount.put(username, attemptCount.getOrDefault(username, 0) + 1);
+        return !userMap.containsKey(username);
+    }
+
+    public void registerUser(String username, int userId) {
+        userMap.put(username, userId);
+    }
+
+    public List<String> suggestAlternatives(String username) {
+        List<String> suggestions = new ArrayList<>();
+
+        for (int i = 1; i <= 5; i++) {
+            String suggestion = username + i;
+            if (!userMap.containsKey(suggestion)) {
+                suggestions.add(suggestion);
+            }
+        }
+
+        String modified = username.replace("_", ".");
+        if (!userMap.containsKey(modified)) {
+            suggestions.add(modified);
+        }
+
+        return suggestions;
+    }
+
+    public String getMostAttempted() {
+        String most = null;
+        int max = 0;
+
+        for (Map.Entry<String, Integer> entry : attemptCount.entrySet()) {
+            if (entry.getValue() > max) {
+                max = entry.getValue();
+                most = entry.getKey();
+            }
+        }
+
+        if (most == null) {
+            return "No attempts yet";
+        }
+
+        return most + " (" + max + " attempts)";
+    }
 }
